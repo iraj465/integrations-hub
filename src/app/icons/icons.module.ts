651018e06d859372cdcf2de4309f3154929869de
iraj465 <saptarshimajumder19@gmail.com>
2021-04-01 22:11:05 +0530

    chore:Updated template-service

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { FeatherModule } from 'angular-feather';
-import { Camera, Heart, Github, ExternalLink, Cpu, Activity, Settings,ArrowRightCircle,Anchor ,Home, Layers} from 'angular-feather/icons';
+import { Camera, Heart, Github, ExternalLink, Cpu, Activity, Settings,ArrowRightCircle,Anchor ,Home, Layers, Box, Star} from 'angular-feather/icons';
 
 // Select some icons (use an object, not an array)
 const icons = {
@@ -15,7 +15,9 @@ const icons = {
   ArrowRightCircle,
   Anchor,
   Home,
-  Layers
+  Layers,
+  Box,
+  Star
 };
 
 @NgModule({
