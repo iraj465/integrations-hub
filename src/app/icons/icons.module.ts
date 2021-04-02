704653f61db467cb86585f53b015491f9052e705
iraj465 <saptarshimajumder19@gmail.com>
2021-04-02 06:41:19 +0530

    fix:Update web-worker manifest

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FeatherModule } from 'angular-feather';
 import { Camera,Clipboard, Heart, Github, ExternalLink, Cpu, Activity, Settings,ArrowRightCircle,Anchor ,Home, Layers, Box, Star, ArrowDownCircle} from 'angular-feather/icons';
 
-// Select some icons (use an object, not an array)
 const icons = {
   Camera,
   Heart,
